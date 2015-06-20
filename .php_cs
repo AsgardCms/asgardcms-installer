b0e4897ed5b4cd8940338e95712f8d34bb6356f5
Nicolas Widart <n.widart@gmail.com>
2015-06-20 23:03:23 +0200

    First Commit

---
+++ .php_cs
@@ -0,0 +1,20 @@
+<?php
+
+$finder = Symfony\CS\Finder\DefaultFinder::create()
+    ->exclude('vendor')
+    ->in(__DIR__)
+;
+
+return Symfony\CS\Config\Config::create()
+    ->setUsingCache(true)
+    ->level(Symfony\CS\FixerInterface::PSR2_LEVEL)
+    ->fixers(array(
+        'concat_with_spaces',
+        'ordered_use',
+        'extra_empty_lines',
+        'remove_lines_between_uses',
+        'return',
+        'unused_use',
+        'whitespacy_lines',
+    ))
+    ->finder($finder);
---
+++ src/NewCommand.php
@@ -0,0 +1,104 @@
+<?php namespace AsgardCms\Installer\Console;
+
+use GuzzleHttp\Client;
+use RuntimeException;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use ZipArchive;
+
+class NewCommand extends Command
+{
+    /**
+     * Configure the command options.
+     */
+    protected function configure()
+    {
+        $this->setName('new')
+            ->setDescription('Create a new AsgardCms application.')
+            ->addArgument('name', InputArgument::REQUIRED);
+    }
+
+    /**
+     * Execute the command.
+     * @param  InputInterface $input
+     * @param  OutputInterface $output
+     * @return void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->verifyApplicationDoesntExist(
+            $directory = getcwd() . '/' . $input->getArgument('name'),
+            $output
+        );
+        $output->writeln('<info>Crafting application...</info>');
+        $this->download($zipFile = $this->makeFilename())
+            ->extract($zipFile, $directory)
+            ->cleanUp($zipFile);
+        $output->writeln('<comment>Application ready! Build something amazing.</comment>');
+    }
+
+    /**
+     * Verify that the application does not already exist.
+     * @param  string $directory
+     * @return void
+     */
+    protected function verifyApplicationDoesntExist($directory, OutputInterface $output)
+    {
+        if (is_dir($directory)) {
+            throw new RuntimeException('Application already exists!');
+        }
+    }
+
+    /**
+     * Generate a random temporary filename.
+     * @return string
+     */
+    protected function makeFilename()
+    {
+        return getcwd() . '/asgardcms_' . md5(time() . uniqid()) . '.zip';
+    }
+
+    /**
+     * Download the temporary Zip to the given file.
+     * @param  string $zipFile
+     * @return $this
+     */
+    protected function download($zipFile)
+    {
+        $response = (new Client)->get('https://github.com/AsgardCms/Platform/archive/1.0.2.zip');
+        file_put_contents($zipFile, $response->getBody());
+
+        return $this;
+    }
+
+    /**
+     * Extract the zip file into the given directory.
+     * @param  string $zipFile
+     * @param  string $directory
+     * @return $this
+     */
+    protected function extract($zipFile, $directory)
+    {
+        $archive = new ZipArchive;
+        $archive->open($zipFile);
+        $archive->extractTo($directory);
+        $archive->close();
+
+        return $this;
+    }
+
+    /**
+     * Clean-up the Zip file.
+     * @param  string $zipFile
+     * @return $this
+     */
+    protected function cleanUp($zipFile)
+    {
+        @chmod($zipFile, 0777);
+        @unlink($zipFile);
+
+        return $this;
+    }
+}
